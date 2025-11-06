38050568f344c1f4602238745ee121f50abec2a5
tohaHack <zakvackijanton@gmail.com>
2025-11-06 21:22:44 +0100

    flask-app-init-fix

--- sportradar_calendar/__init__.py
+++ sportradar_calendar/__init__.py
@@ -9,4 +9,29 @@ def create_app():
     from . import db
     db.init_app(app=app)
 
+    # Event
+    from . import event
+    app.add_url_rule('/add', 'event.add',  event.routes.add_view, methods=['GET', 'POST'])
+    app.add_url_rule('/', 'event', event.routes.get_all_view, methods=['GET'])
+    app.add_url_rule('/delete/<id>', 'event.delete', event.routes.delete, methods=['DELETE'])
+
+    # Sport
+    from . import sport
+    app.add_url_rule('/sport/add', 'sport.add',  sport.routes.add_view, methods=['GET', 'POST'])
+    app.add_url_rule('/sport', 'sport', sport.routes.get_all_view, methods=['GET'])
+    app.add_url_rule('/sport/delete/<id>', 'sport.delete', sport.routes.delete, methods=['DELETE'])
+
+    # Team
+    from . import team
+    app.add_url_rule('/team/add', 'team.add',  team.routes.add_view, methods=['GET', 'POST'])
+    app.add_url_rule('/team', 'team', team.routes.get_all_view, methods=['GET'])
+    app.add_url_rule('/team/delete/<id>', 'team.delete', team.routes.delete, methods=['DELETE'])
+
+    # Venue
+    from . import venue
+    app.add_url_rule('/venue/add', 'venue.add',  venue.routes.add_view, methods=['GET', 'POST'])
+    app.add_url_rule('/venue', 'venue', venue.routes.get_all_view, methods=['GET'])
+    app.add_url_rule('/venue/delete/<id>', 'venue.delete', venue.routes.delete, methods=['DELETE'])
+
+
     return app
